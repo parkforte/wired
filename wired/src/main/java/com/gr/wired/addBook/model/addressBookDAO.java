becdb6b878d0bcca18ab07c71f8926e2409fd198
Kanghui_kim <alsrnr0162@naver.com>
2022-01-07 09:53:54 +0900

    kong - 주소록 연락처 연동

--- wired/src/main/java/com/gr/wired/addBook/model/addressBookDAO.java
+++ wired/src/main/java/com/gr/wired/addBook/model/addressBookDAO.java
@@ -1,5 +1,19 @@
 package com.gr.wired.addBook.model;
 
-public interface addressBookDAO {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gr.wired.common.SearchVO;
+
+@Mapper
+public interface addressBookDAO {
+	int insertAddressBook(addressBookVO addressBookVo);
+	String selectPwd(String memId);
+	addressBookVO selectByMemId(String memId);
+	List<Map<String, Object>> selectAll(SearchVO searchVo);
+	int selectTotalRecord(SearchVO searchVo);
+	int updateSignature(addressBookVO addressBookVo);
+	List<Map<String, Object>> selectByMemName(addressBookVO addressBookVo);
 }
