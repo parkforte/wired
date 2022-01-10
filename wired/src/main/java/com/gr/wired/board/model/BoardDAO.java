e37d062f32ca6cec35a2807b279b6f1ad5a0f729
myoungsikkim <94949302+myoungsikkim@users.noreply.github.com>
2022-01-10 12:43:59 +0900

    명식 게시글 삭제완료

--- wired/src/main/java/com/gr/wired/board/model/BoardDAO.java
+++ wired/src/main/java/com/gr/wired/board/model/BoardDAO.java
@@ -5,15 +5,17 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.gr.wired.common.SearchVO;
+
 @Mapper
 public interface BoardDAO {
 	int insertBoard(BoardVO boardVo);
-	List<Map<String, Object>> selectByBNoList(int bdlistNo);
+	List<Map<String, Object>> selectByBNoList(SearchVO searchVo);
 	int selectTotalRecord(int boardNo);
 	int selectByMemId(String memId);
 	Map<String, Object> selectByNo (int boardNo);
 	int updateBoard(BoardVO boardVo);
 	int updateReadCount(int boardNo);
 	int updateRecommend(int boardNo);
-	int deleteBoard(BoardVO boardVo);
+	int deleteBoard(int boardNo);
 }
