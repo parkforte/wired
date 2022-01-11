b6ead59a378b01e87e88691cb2db68c523f21e26
parkforte <parkforte@naver.com>
2022-01-11 23:50:59 +0900

    전자결재 인서트, 업데이트

--- wired/src/main/java/com/gr/wired/confirm/model/ConfirmService.java
+++ wired/src/main/java/com/gr/wired/confirm/model/ConfirmService.java
@@ -3,5 +3,6 @@ package com.gr.wired.confirm.model;
 public interface ConfirmService {
 	int insertPaper(ConfirmVO confirmVo);
 	ConfirmVO selectTempByMemNo(int memNo);
+	int updateContent(ConfirmVO confirmVo);
 
 }
